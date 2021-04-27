11a6c53531b9fe990cb99fc47a49f373deca3d2e
Paweł Stachel <p.stachel@qarbon.pl>
2021-04-28 00:51:23 +0200

    new carousel home

--- themes/zig-template/js/index.js
+++ themes/zig-template/js/index.js
@@ -3,8 +3,37 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.header__nav');
 const header = document.querySelector('.header');
+const carousel_post = document.querySelectorAll('time.entry-date');
+
+const setOpacityOnLastPost = () => {
+  setInterval(() => {
+    const res = document.querySelectorAll('.slick-slide.slick-active');
+    if (res && res.length > 1) {
+      const last = res.length - 1;
+      const one_before_last = last > 1 ? last - 1 : 0;
+      if (!res[last].classList.contains('semiopaque') || res[one_before_last].classList.contains('semiopaque')) {
+        res.forEach(el => {
+          el.classList.remove('semiopaque');
+        });
+        res[last].classList.add('semiopaque');
+      }
+    }
+  }, 100);
+};
+
+const formatTime = () => {
+  if (carousel_post && carousel_post.length) {
+    carousel_post.forEach((post) => {
+      const t = post.attributes.datetime.nodeValue;
+      const date = new Date(t);
+      post.innerHTML = date.toLocaleDateString();
+    });
+  }
+};
 
 hamburger.addEventListener('click', () => handleHamburgerClick());
+formatTime();
+setOpacityOnLastPost();
 
 if (window.location.pathname === '/dolacz/') {
   header.classList.add('inverted');
