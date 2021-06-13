65074ee71ad1504d9bd4ff900c9bab95dbdf36c7
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2021-06-13 20:37:33 +0200

    dynamic carousels fix

--- themes/zig-template/single.php
+++ themes/zig-template/single.php
@@ -28,27 +28,42 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 	  <?php the_content(); ?>
   </div>
 </div>
+
+<?php if ( strpos( $url, 'czlonkowie' ) === false ) {?>
+
+  <section class="carousel padding-l">
+    <div class="carousel-title">
+      <p>Sporo się u nas dzieje.</p>
+      <p>Bądź zawsze na bieżąco </p>
+    </div>
+	  <?php echo do_shortcode( '[sp_wpcarousel id="395"]' ); ?>
+    <a href="/category/wszystkie">
+      <button class="button grenade">ZOBACZ WSZYSTKIE</button>
+    </a>
+  </section>
+
+
+<?php } else { ?>
+
+
 <div class="society">
   <div class="padding-l">
     <div class="subpage-header">
-		<?php
-		if ( strpos( $url, 'czlonkowie' ) === false ) {
-			?>
-          Bądź zawsze na bieżąco
-		<?php } else { ?>
-          Jest nas więcej
-		<?php } ?>
+      Jest nas więcej
     </div>
     <div class="carousel about">
-		<?php
-		if ( strpos( $url, 'czlonkowie' ) === false ) {
-			echo do_shortcode( '[sp_wpcarousel id="395"]' );
-		} else {
-			echo do_shortcode( '[sp_wpcarousel id="514"]' );
-		} ?>
+		<?php echo do_shortcode( '[sp_wpcarousel id="514"]' ); ?>
     </div>
   </div>
 </div>
+
+<?php } ?>
+
+
+
+
+
+
 <?php
 get_footer();
 ?>
